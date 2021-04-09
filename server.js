468c729443443a92a7d37a0639865af17fb3eecf
zixiqin <80296726+zixiqin@users.noreply.github.com>
2021-04-09 11:59:58 +1000

    lydia first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+// localhost:5000/
+
+app.get('/vendor', (req, res) => {
+    res.status(200).json({ sucess: "true", message: "what the hell is the world" })
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
