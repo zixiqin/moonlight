2390c50688134d6228ff1b779fee1239133e2c75
Zixi Qin <zixiq@student.unimelb.edu.au>
2021-04-09 17:51:55 +1000

    add 8888

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const port = 5000;
 app.get('/vendor', (req, res) => {
     res.status(200).json({ sucess: "true", message: "what the hell is the world" })
     console.log("a good thing")
+    console.log("bad day right?")
+
+    console.log("bad day 888888888?")
 })
 
 app.listen(port, () => {
