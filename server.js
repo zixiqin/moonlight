bf0fa086e281ad494fb6199c10bdb54d3bbb6e66
Zixi Qin <zixiq@student.unimelb.edu.au>
2021-04-09 22:52:11 +1000

    2103 conflict merge

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 5000;
-// localhost:5000/
-
 app.get('/customer', (req, res) => {
     res.status(200).json({ sucess: "true", message: "what the hell is the world" })
 })
