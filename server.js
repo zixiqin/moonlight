967dcb02ada5e073518d88da99656f6fcfd7d479
Zixi Qin <zixiq@student.unimelb.edu.au>
2021-04-10 09:39:12 +1000

    2103 heroku setting

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 // localhost:5000/
 
-app.get('/customer', (req, res) => {
-        res.status(200).json({ sucess: "true", message: "what the hell is the world" })
-    })
-    //manu
-app.get('/login', (req, res) => {
-    res.status(200).json({ sucess: "true", message: "Log in page" })
+app.get('/', (req, res) => {
+    res.status(200).json({ sucess: "true", message: "what the hell is the world" })
 })
 
-app.get('/okkkk', (req, res) => {
-    res.status(200).json({ sucess: "true", message: "Log in page" })
-})
-
-//shjsdgidcxbic
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
