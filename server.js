ad9dd2133e7d31a6f8734842631662b906063a2a
Zixi Qin <zixiq@student.unimelb.edu.au>
2021-04-09 22:30:37 +1000

    2103 login page

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const port = 5000;
 
 app.get('/vendor', (req, res) => {
     res.status(200).json({ sucess: "true", message: "what the hell is the world" })
-    console.log("a good thing")
-    console.log("bad day right?")
-
-    console.log("bad day 888888888?")
-    console.log("bad day 99999?")
 })
+
+app.get('/login', (req, res) => {
+    res.status(200).json({ sucess: "true", message: "Log in page" })
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
