ab49eec3606bb809c93c9e8f5e3737625200e80a
my_z <menzhang1@student.unimelb.edu.au>
2021-05-12 17:04:06 +1000

    链接socket.io 50:00

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 const mongoose = require('mongoose')
 
+const server = require('http').createServer(app);
+const io = require("socket.io")(server);
+
 const vendor = require('./routes/vendor');
 const snack = require('./routes/snack');
 const customer = require('./routes/customer');
@@ -34,10 +37,50 @@ app.get('/', (req, res) => {
 
 
 app.use(bodyParser.json());
+
+io.of("/api/spcket").on("connection", (socket) => {
+    console.log("socket.io: User connected: ", socket.id);
+    socket.on("disconnect", () => {
+        console.log("socket.io: User disconnected: ", socket.id);
+    });
+});
+
 const db = require('./dtbase/keys').mongoURL;
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Yay! MongoDB Has Connected..."))
+    .catch((err) => console.log(err));
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("Setting change streams");
+    const orderChangeStream = connection.collection("orders").watch();
+
+    orderChangeStream.on("change", (change) => {
+        switch (change.operationType) {
+            case "insert":
+                console.log("insertion detected at backend");
+                const order = {
+                    _id: change.fullDocument._id,
+                    customer: change.fullDocument.customer,
+                    vendor: change.fullDocument.vendor,
+                    snacks: change.fullDocument.snacks,
+                    createAt: change.fullDocument.createAt
+                };
+                io.of("/api/socket").emit("newOrder", order);
+                break;
+            case "update":
+                consolde.log("update detected at backend");
+                io.of("/api/socket").emit("updateOrder", change.documentKey._id);
+                break;
+            
+            case "delete":
+                consolde.log("deletion detected at backend");
+                io.of("/api/socket").emit("deleteOrder", change.documentKey._id);
+                break;
+        }
+    })
+})
 
 // add the route for three sections
 app.use('/vendor', vendor);
@@ -48,6 +91,6 @@ app.use('/order', order);
 
 
 // url listen..
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
