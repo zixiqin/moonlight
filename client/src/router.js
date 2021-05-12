49c71e1ed73ccb4d79aa2f98253c007bcffa5f2c
Zixi Qin <zixiq@student.unimelb.edu.au>
2021-05-12 10:18:55 +1000

    basic file create/06:44

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from  'react-router-dom';
 import App from './pages/App.js';
 import CustomerMain from './pages/CustomerMain.js';
+import CustomerProfile from './pages/CustomerProfile.js';
+import VendorPark from './pages/VendorPark.js';
+import VendorOrders from './pages/VendorOrders';
 
 class Router extends React.Component{
     render() {
@@ -10,6 +13,9 @@ class Router extends React.Component{
                 <Switch>
                     <Route path="/" exact component={App}></Route>
                     <Route path="/customer" exact component={CustomerMain}></Route>
+                    <Route path="/profile" exact component={CustomerProfile}></Route>
+                    <Route path="/vendor" exact component={VendorPark}></Route>
+                    <Route path="/orders" exact component={VendorOrders}></Route>
                 </Switch>
             </BrowserRouter>
         )
