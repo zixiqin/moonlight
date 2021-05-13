d7beae13eeabf853f337aa77a81160e6f62b9723
Zixi Qin <zixiq@student.unimelb.edu.au>
2021-05-13 20:13:50 +1000

    01:41:29 员工可以查看订单

--- client/src/pages/VendorOrders.js
+++ client/src/pages/VendorOrders.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-export default function VendorOrders() {
+import {Row, Button} from 'antd'
+
+import OrderList  from '../components/OrderList';
+import Header from '../components/Header';
+
+export default function VendorOrders(props) {
+
+
+    const [target, setTarget] = useState('');
+    const [status, setStatus] = useState('');
+
+    useEffect(()=>{
+        if(window.location.pathname === '/orders'){
+            setTarget('vendor')
+        }
+    }, [])
+
+
+
+
     return (
         <div>
-            
+            <Header vendor = {props.location.state.vendor} />
+            <div style = {{marginLeft:'3vw', alignItems:'center'}}>
+                <Row gutter={6}>
+                    <Button onClick={()=> setStatus('&status=outstanding')}>Outstanding</Button>
+                    <Button onClick={()=> setStatus('&status=fulfilled')} style={{marginLeft:'1vw'}}>Fulfilled</Button>
+                    <Button onClick={()=> setStatus('&status=completed')} style={{marginLeft:'1vw'}}>Completed</Button>
+                </Row>
+
+            </div>
+            <OrderList id={props.location.state.vendor.id} target={target} status = {status} />
         </div>
     )
 }
